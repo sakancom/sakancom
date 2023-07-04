f4cb847064fcf47f1782f780246e5d0136214b36
Alaa <s12029009@stu.najah.edu>
2023-07-04 23:18:20 +0300

    ;new commit

--- src/test/java/sakancom/loginfeaturesteps.java
+++ src/test/java/sakancom/loginfeaturesteps.java
@@ -10,17 +10,32 @@ public class loginfeaturesteps {
     @Given("you are in login page")
     public void you_are_in_login_page() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+     //   throw new io.cucumber.java.PendingException();
     }
     @When("I type in email {string} And I type in password {string}")
     public void i_type_in_email_and_i_type_in_password(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+      //  throw new io.cucumber.java.PendingException();
     }
+    @Then("I should see E-mail or password is incorrect")
+    public void iShouldSeeEMailOrPasswordIsIncorrect() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+
     @Then("I should see Access your account")
     public void i_should_see_access_your_account() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        // throw new io.cucumber.java.PendingException();
     }
+
+    @Then("Empty password or username")
+    public void emptyPasswordOrUsername() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+
+
+
 
 }
---
+++ src/test/java/sakancom/signupfeaturesteps.java
@@ -0,0 +1,42 @@
+package sakancom;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class signupfeaturesteps {
+
+    @Given("you are in signup page")
+    public void you_are_in_signup_page() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+    @When("you write the {string}, {string}, {string}, {string}, {string}, {string}, {string} and {string}")
+    public void you_write_the_and(String string, String string2, String string3, String string4, String string5, String string6, String string7, String string8) {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("you should show please fill all informations")
+    public void you_should_show_please_fill_all_informations() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("You should Show please enter another id")
+    public void youShouldShowPleaseEnterAnotherId() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("You should Show please enter more than five character")
+    public void youShouldShowPleaseEnterMoreThanFiveCharacter() {
+        // Write code here that turns the phrase above into concrete actions
+       // throw new io.cucumber.java.PendingException();
+    }
+    @Then("The Customer Added Successfully")
+    public void theCustomerAddedSuccessfully() {
+        // Write code here that turns the phrase above into concrete actions
+       // throw new io.cucumber.java.PendingException();
+    }
+
+}
